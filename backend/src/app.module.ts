c5ca78d8a32040a9fcc0dd3168b66069d77de6d3
eitikobata <eiti.kun@gmail.com>
2026-08-06 02:23:06 -0300

    chore(app): wire IncidentsModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { RedisModule } from './redis/redis.module';
 import { EventsModule } from './events/events.module';
 import { CorrelationModule } from './correlation/correlation.module';
+import { IncidentsModule } from './incidents/incidents.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { CorrelationModule } from './correlation/correlation.module';
     RedisModule,
     EventsModule,
     CorrelationModule,
+    IncidentsModule,
   ],
 })
 export class AppModule {}
